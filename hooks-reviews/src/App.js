6aa8f7befd75f04704a3ddc6ccb907187813c524
0sorojo <redrootproductions@gmail.com>
2020-10-16 19:53:10 -0400

    hooks index management reviews completed

--- hooks-reviews/src/App.js
+++ hooks-reviews/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Review from './Review';
+import { FaGithubAlt } from 'react-icons/fa';
 
 const App = () => {
   return (
-    <>
-      <h1>I work</h1>
-    </>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <FaGithubAlt className='icon' />
+          <h2>Our Reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 };
 
