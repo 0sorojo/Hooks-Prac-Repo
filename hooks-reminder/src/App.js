8bbe079acb9ca442d55cdfce961d9e50d4404a30
0sorojo <redrootproductions@gmail.com>
2020-10-16 10:36:13 -0400

    finished project1

--- hooks-reminder/src/App.js
+++ hooks-reminder/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import List from './List';
+import data from './data';
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
+
   return (
-    <>
-      <h1>I work</h1>
-    </>
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthday today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
