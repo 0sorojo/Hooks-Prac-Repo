97756f83a6dd17eac91fe409ce7d17a7d657c2a0
0sorojo <redrootproductions@gmail.com>
2020-10-19 13:16:03 -0400

    dynamic menu complete with button mapped to unique set of properties for easy navigation

--- hooks-dynamic-menu/src/App.js
+++ hooks-dynamic-menu/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Categories from './Categories';
+import items from './data';
+import Menu from './Menu';
 
-function App() {
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filteredItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <>
-      <h1>I work</h1>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} filteredItems={filteredItems} />
+        <Menu items={menuItems} />
+      </section>
     </>
   );
-}
+};
 
 export default App;
---
+++ hooks-dynamic-menu/src/Categories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Categories = ({ categories, filteredItems }) => {
+  return (
+    <>
+      <div className='btn-container'>
+        {categories.map((category, index) => {
+          return (
+            <button
+              type='button'
+              className='filter-btn'
+              key={index}
+              onClick={() => filteredItems(category)}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
